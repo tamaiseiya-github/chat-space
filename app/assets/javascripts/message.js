1d77f77d23d3429e974f34943e7207f76feffd60
tamaiseiya-github <tamaiphone7@gmail.com>
2020-02-03 19:06:00 +0900

    リセット

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,44 @@
 $(function(){
-  var buildHTML = function(message){
+  function buildHTML(message){
     if (message.image){
       var html =  
-        `<div class = "message" data-message-id=${message.id}>
-          <div class="message__info">
-            <div class="message__info__member">
+        `<div class="messages__message" data-message-id=${message.id}>
+          <div class="messages__message__info">
+            <div class="messages__message__info__member">
               ${message.user_name}
             </div>
-            <div class="message__info__date">
+            <div class="messages__message__info__date">
               ${message.created_at}
             </div>
           </div>
-          <div class="message__text">
-            <p class="message__text__content">
+          <div class="messages__message__text">
+            <p class="messages__message__text__content">
               ${message.content}
             </p>
           </div>
           <img src=${message.image} >
         </div>`
-        return html;
+      return html;
     } else {
       var html =
-        `<div class = "message" data-message-id=${message.id}>
-          <div class="message__info">
-            <div class="message__info__member">
-              ${message.user_name}
-            </div>
-            <div class="message__info__date">
-              ${message.created_at}
-            </div>
+        `<div class="messages__message" data-message-id=${message.id}>
+        <div class="messages__message__info">
+          <div class="messages__message__info__member">
+            ${message.user_name}
           </div>
-          <div class="message__text">
-            <p class="message__text__content">
-              ${message.content}
-            </p>
+          <div class="messages__message__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="messages__message__text">
+          <p class="messages__message__text__content">
+            ${message.content}
+          </p>
         </div>`
-        return html;
+      return html;
     };
   }
-
-    $('.new_message').on('submit', function(e){
+  $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -53,7 +52,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.middle').append(html);
+      $('.messages').append(html);
       $('form')[0].reset();
       $('.middle').animate({ scrollTop: $('.middle')[0].scrollHeight},'fast');
       $('input').prop('disabled', false);
@@ -62,32 +61,4 @@ $(function(){
       alert('メッセージ送信に失敗しました');
     })
   })
-  
-  var reloadMessages = function() {
-    last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.middle').append(insertHTML);
-        $('.middle').animate({ scrollTop: $('.middle')[0].scrollHeight});
-        $("#new_message")[0].reset();
-        $(".form__submit").prop("disabled", false);
-      }
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    });
-  };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "groups#index"
-  resources :users, only: [:index,:edit, :update]
+  resources :users, only: [:edit, :update]
   resources :groups, only: [:edit, :update, :new, :create] do
     resources :messages, only: [:index, :create]
-    namespace :api do
-      resources :messages, only: :index, defaults: { format: 'json' }
-    end
   end
 end
